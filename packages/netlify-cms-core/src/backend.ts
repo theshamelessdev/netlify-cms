b4bb2c901306681391b03be3ed25b389a636873b
erezrokah <erezrokah@users.noreply.github.com>
2020-04-29 18:59:51 +0300

    feat(backend-github): support moving entries

--- packages/netlify-cms-core/src/backend.ts
+++ packages/netlify-cms-core/src/backend.ts
@@ -734,6 +734,7 @@ export class Backend {
     unpublished = false,
     status,
   }: PersistArgs) {
+    const useWorkflow = selectUseWorkflow(config);
     const newEntry = entryDraft.getIn(['entry', 'newRecord']) || false;
 
     const parsedData = {
@@ -746,7 +747,6 @@ export class Backend {
       slug: string;
       raw: string;
       newPath?: string;
-      oldSlug?: string;
     };
 
     const customPath = getCustomPath(collection, entryDraft);
@@ -782,9 +782,11 @@ export class Backend {
         asset.path = newPath;
       });
     } else {
+      const slug = entryDraft.getIn(['entry', 'slug']);
       entryObj = {
         path: entryDraft.getIn(['entry', 'path']),
-        slug: entryDraft.getIn(['entry', 'slug']),
+        // for workflow entries we refresh the slug on publish
+        slug: customPath && !useWorkflow ? slugFromCustomPath(collection, customPath) : slug,
         raw: this.entryToRaw(collection, entryDraft.get('entry')),
         newPath: customPath,
       };
@@ -803,8 +805,6 @@ export class Backend {
       },
       user.useOpenAuthoring,
     );
-
-    const useWorkflow = selectUseWorkflow(config);
 
     const collectionName = collection.get('name');
 
